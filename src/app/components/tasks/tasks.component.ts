2d452a51568b9f6796297042bfbc3861f5ca894f
Timo Kivinen <Timo.Kivinen@student.lut.fi>
2024-03-29 22:32:01 +0200

    Video: 49:15

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {Task} from '../../Task' ;
-import {TASKS} from '../../mock-tasks' ;
+import {TaskService} from '../../services/task.service';
+import {Task} from '../../Task';
+
+// import {TASKS} from '../../mock-tasks';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
+
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  constructor() { }
+  // tasks: Task[] = TASKS;
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+
   }
 
 }
