39fc49b07f69f4f0d051969c8bfe17b6d55af9f7
Timo Kivinen <Timo.Kivinen@student.lut.fi>
2024-03-30 21:21:32 +0200

    Video: 1:28:30

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+
+export class AddTaskComponent implements OnInit {
+  text: string | undefined;
+  day: string | undefined;
+  reminder: boolean=false;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
