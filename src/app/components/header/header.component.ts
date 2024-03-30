73aa87004218b50dd8185549a7814f1efe2b1620
Timo Kivinen <Timo.Kivinen@student.lut.fi>
2024-03-31 01:52:36 +0200

    Video: 2:01:31

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UiService} from '../../services/ui.service';
 import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -17,7 +18,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = false;
   subscription!: Subscription;
 
-  constructor(private uiService:UiService) {
+  constructor(private uiService:UiService, private router:Router) {
     this.subscription = this.uiService
       .onToggle()
       .subscribe((value) => (this.showAddTask = value));
@@ -29,5 +30,7 @@ export class HeaderComponent implements OnInit {
     //// console.log('header.component: toggle**');
     this.uiService.toggleAddTask();
   }
-
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
 }
