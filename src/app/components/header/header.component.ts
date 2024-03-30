c5f8add1051653c4419e42b430b67f8445bed055
Timo Kivinen <Timo.Kivinen@student.lut.fi>
2024-03-31 01:16:16 +0200

    Video: 1:48:00

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {UiService} from '../../services/ui.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
   // --- Moved from appComponent.ts
   title: string = 'Task Tracker';
-  constructor() { }
+  //showAddTask: boolean;
+  ///subscription: Subscription;
+
+  showAddTask: boolean = false;
+  subscription!: Subscription;
+
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {
   }
   toggleAddTask() {
-    console.log('toggle');
+    //// console.log('header.component: toggle**');
+    this.uiService.toggleAddTask();
   }
 
 }
